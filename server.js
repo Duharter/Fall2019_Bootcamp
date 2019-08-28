5c1d2296ab080178e96dc76cda1cc75d28c95cad
Duharter <duharter@ufl.edu>
2019-08-28 14:02:17 -0400

    Finished

--- server.js
+++ server.js
@@ -8,44 +8,29 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
-};
+  if(request.method === 'GET' && parsedUrl.pathname === '/listings'){
+    response.end(JSON.stringify(listingData, null, 2));
+    console.log(request.url);
+  }
+  else
+  {
+    response.statusCode = 404;
+    console.log('statusCode:', response && response.statusCode);
+    console.log('Bad gateway error');
+    response.write('Bad gateway error');
+    response.end();
+  }
+}
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  if(err) throw err;
 
-    HINT: Read up on JSON parsing Node.js
-   */
+  listingData = JSON.parse(data);
+    console.log('server listening on: http://localhost:8080');
 
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
+    server = http.createServer(requestHandler);
+    server.listen(port);
 
 
 });
