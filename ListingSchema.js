bd0c363009202111ecc2cc3ec0ec7fd1636b4720
Duharter <duharter@ufl.edu>
2019-09-10 12:45:24 -0400

    Yeet

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,42 +1,41 @@
+/* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
+/* Create your schema */
 var listingSchema = new Schema({
-  code:{
-    type: String,
-    required: true,
+  name: {
+    type: String, 
+    required: true
+  }, 
+  code: {
+    type: String, 
+    required: true, 
+    unique: true
+  }, 
+  address: String, 
+  coordinates: {
+    latitude: Number, 
+    longitude: Number
   },
-  name:{
-    type: String,
-    requried:true,
-  },
-  coordinate:{
-    latitude: Number,  
-    longitude: Number,
-  },
-  address: String,
-  updated_at: Date,
-  created_at: Date
+  created_at: Date,
+  updated_at: Date
 });
 
-
+/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-
-  var currentDate = new Date();
-
-  this.updated_at = currentDate;
+  var currentTime = new Date;
+  this.updated_at = currentTime;
   if(!this.created_at)
-    this.created_at = currentDate;
-
-    if(typeof code!= String){
-      throw err;
-    }
-    else if(typeof name!= String){
-      throw err;
-    }
-    else
+  {
+    this.created_at = currentTime;
+  }
   next();
 });
 
+/* Use your schema to instantiate a Mongoose model */
 var Listing = mongoose.model('Listing', listingSchema);
+
+/* Export the model to make it avaiable to other parts of your Node application */
+>>>>>>> Stashed changes
 module.exports = Listing;
